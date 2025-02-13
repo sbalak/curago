a93d7463623786b4cccf36fa1e116ff7bcdd5199
nikitagupta5566 <33730926+nikitagupta5566@users.noreply.github.com>
2025-02-13 18:06:40 +0000

    Adding dashboard component for taking specialities and symptoms input

--- patient-client/app/(protected)/dashboard/_layout.tsx
+++ patient-client/app/(protected)/dashboard/_layout.tsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import { Stack } from 'expo-router'
-import DashboardHeader from '@/components/headers/DashboardHeader'
+import React from "react";
+import { Stack } from "expo-router";
+import DashboardHeader from "@/components/headers/DashboardHeader";
+
+// export default function _layout() {
+//   return (
+//     <Stack
+//       screenOptions={({ route }) => ({
+//         console.log('Header Title:', route.params?.headerTitle);
+//         headerTitle: route.params?.headerTitle ?? 'Default Title',
+//         headerTitleStyle: {
+//           fontFamily: 'outfit-bold',
+//           fontSize: 20
+//         }
+//       })}
+//     >
+//       <Stack.Screen name="index" options={{ header: () => <DashboardHeader /> }} />
+//     </Stack>
+//   )
+// }
 
 export default function _layout() {
   return (
-    <Stack 
-      screenOptions={({ route }) => ({
-        headerTitle: route.params.headerTitle,
-        headerTitleStyle: {
-          fontFamily: 'outfit-bold',
-          fontSize: 20
-        } 
-      })} 
+    <Stack
+      screenOptions={({ route }) => {
+        console.log("Header Title:", route.params?.headerTitle);
+
+        return {
+          headerTitle: route.params?.headerTitle ?? "Default Title",
+          headerTitleStyle: {
+            fontFamily: "outfit-bold",
+            fontSize: 20,
+          },
+        };
+      }}
     >
-      <Stack.Screen name="index" options={{ header: () => <DashboardHeader /> }} />
+      <Stack.Screen
+        name="index"
+        options={{ header: () => <DashboardHeader /> }}
+      />
     </Stack>
-  )
+  );
 }
